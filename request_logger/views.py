0c521c5e22d69137caabd7bc362b0692e6206791
Nuno Rosa <rnunojanela@gmail.com>
2016-04-28 13:44:06 +0100

    Tweek stuff

--- request_logger/views.py
+++ request_logger/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pprint import pformat
 
 from flask import request
@@ -14,6 +15,37 @@ class RegexConverter(BaseConverter):
     self.regex = items[0]
 
 
+class WSGICopyBody(object):
+  def __init__(self, application):
+    self.application = application
+
+  def __call__(self, environ, start_response):
+
+    from cStringIO import StringIO
+    length = environ.get('CONTENT_LENGTH', '0')
+    length = 0 if length == '' else int(length)
+
+    body = environ['wsgi.input'].read(length)
+    environ['body_copy'] = body
+    environ['wsgi.input'] = StringIO(body)
+
+    # Call the wrapped application
+    app_iter = self.application(environ,
+                                self._sr_callback(start_response))
+
+    # Return modified response
+    return app_iter
+
+  def _sr_callback(self, start_response):
+    def callback(status, headers, exc_info=None):
+
+      # Call upstream start_response
+      start_response(status, headers, exc_info)
+    return callback
+
+app.wsgi_app = WSGICopyBody(app.wsgi_app)
+
+
 app.url_map.converters['regex'] = RegexConverter
 
 stored_requests = []
@@ -21,10 +53,13 @@ stored_requests = []
 
 @app.route('/<regex(".+"):url>', methods=['GET', 'POST'])
 def index(url):
-  stored_requests.append({
-    'url': '{} {}'.format(request.method, request.url),
-    'data': pformat(vars(request), indent=2),
-  })
+  if url != 'favicon.ico':
+    stored_requests.append({
+      'timestamp': datetime.now(),
+      'url': '{} {}'.format(request.method, request.url),
+      'data': pformat(vars(request), indent=2),
+    })
+
   return render_template(
     'index.html',
     stored_requests=stored_requests,
@@ -35,5 +70,5 @@ def index(url):
 def display_calls():
   return render_template(
     'index.html',
-    stored_requests=stored_requests,
+    stored_requests=sorted(stored_requests, key=lambda x: x['timestamp']),
   )
